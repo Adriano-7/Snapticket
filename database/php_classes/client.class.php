99f41482a6f1882fd03ca3408497eac8d646c184
Adriano Machado <93844395+Adriano-7@users.noreply.github.com>
2023-04-21 19:24:40 +0100

    Changed class names to increase readability

---
+++ database/php_classes/client.class.php
@@ -0,0 +1,33 @@
+<?php 
+    declare(strict_types = 1);
+    class Client {
+        public string $name;
+        public string $username;
+        public string $email;
+        public string $password;
+
+        public function __construct(string $name, string $username, string $email, string $password) {
+            $this->name = $name;
+            $this->username = $username;
+            $this->email = $email;
+            $this->password = $password;
+        }
+        
+        static function getClientWithPassword(PDO $db, string $username, string $password) : ?Client{
+            $query = $db->prepare('
+            SELECT name, username, email, password
+            FROM Client
+            WHERE lower(username) = ? AND password = ?
+          ');
+    
+          $query->execute(array(strtolower($username), sha1($password)));
+
+          if($client = $query->fetch()){
+            return new Client($client['name'], $client['username'], $client['email'], $client['password']);
+          }
+          return null;
+        }
+    }
+
+
+?>
