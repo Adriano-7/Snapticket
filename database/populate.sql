f399b8ed1f53696a1f4cb8ee5623eb9b10c64e3c
hmgc2002 <hmgc2016@gmail.com>
2023-04-21 15:19:29 +0100

    Error fixing

--- database/populate.sql
+++ database/populate.sql
@@ -14,7 +14,7 @@ INSERT INTO Client VALUES ('Andrew Peterson', 'APeterson12', 'peterson_andrew@sn
 INSERT INTO Client VALUES ('Emily Davis', 'EmilyDavis', 'EmilyDavis15@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
 INSERT INTO Client VALUES ('Adam Green', 'AGreen', 'Agreen@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
 INSERT INTO Client VALUES ('Jessica Ramirez', 'JessicaRamirez15', 'jessica_ramirez@snapticket.com','7c4a8d09ca3762af61e59520943dc26494f8941b');
-INSERT INTO Client VALUES ('Daniel Wilson', 'DWilson14', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Daniel Wilson', 'DWilson14', 'DWilson14@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
 INSERT INTO Client VALUES ('James Davis', 'JamesDavis', 'james_davis7@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
 INSERT INTO Client VALUES ('Jessica Murphy', 'JMurphy', 'jessica_murphy@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
 INSERT INTO Client VALUES ('Michael Brown', 'MBrown1', 'michael_brown24@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
@@ -34,7 +34,7 @@ INSERT INTO Agent VALUES ('MBrown1');
 INSERT INTO Agent VALUES ('SarahJohnson7');
 INSERT INTO Agent VALUES ('CChen');
 INSERT INTO Agent VALUES ('SKim');
-INSERT INTO Agent VALUES ('APat');
+INSERT INTO Agent VALUES ('APatel');
 INSERT INTO Agent VALUES ('MCooper');
 INSERT INTO Agent VALUES ('EChen');
 INSERT INTO Agent VALUES ('BenjCollins');
@@ -47,13 +47,13 @@ INSERT INTO Admin VALUES ('JMurphy');
 INSERT INTO Ticket VALUES (540, 'Need access to financial reports', '2023-05-13', '', 'SarahJohnson7', 'Assigned', 'APeterson12');
 INSERT INTO Ticket VALUES (541, 'Incorrect tax filling submitted to IRS for client', '2023-05-13', '', 'MBrown1', 'Assigned', 'APeterson12');
 INSERT INTO Ticket VALUES (542, 'Issue with depreciation calculation for fixed assets', '2023-05-13', '', 'SarahJohnson7', 'Assigned', 'APeterson12');
-INSERT INTO Ticket VALUES (543, 'Issue with accounts payable aging report', '2023-05-13', '', '', 'Open', 'APeterson12');
+INSERT INTO Ticket VALUES (543, 'Issue with accounts payable aging report', '2023-05-13', '', NULL, 'Open', 'APeterson12');
 INSERT INTO Ticket VALUES (544, 'Inquiry about accounting treatment for leasehold improvements', '2023-05-13', '', 'CChen', 'Closed', 'APeterson12');
-INSERT INTO Ticket VALUES (545, 'Issue with expense reimbursement for employee travel', '2023-05-13', '', '', 'Open', 'APeterson12');
+INSERT INTO Ticket VALUES (545, 'Issue with expense reimbursement for employee travel', '2023-05-13', '', NULL, 'Open', 'APeterson12');
 INSERT INTO Ticket VALUES (546, 'Request for assistance with financial due diligence', '2023-05-13', '', 'JamesDavis', 'Closed', 'APeterson12');
 INSERT INTO Ticket VALUES (547, 'Unable to reconcile bank accounts in QuickBooks', '2023-05-13', '', 'SKim', 'Assigned', 'APeterson12');
-INSERT INTO Ticket VALUES (548, 'Having trouble connecting to the company email', '2023-05-13', '', 'APat', 'Assigned', 'APeterson');
-INSERT INTO Ticket VALUES (549, 'Issue with payroll processing', '2023-05-13', '', 'CChen', 'Assigned', 'APeterson');
+INSERT INTO Ticket VALUES (548, 'Having trouble connecting to the company email', '2023-05-13', '', 'APatel', 'Assigned', 'APeterson12');
+INSERT INTO Ticket VALUES (549, 'Issue with payroll processing', '2023-05-13', '', 'CChen', 'Assigned', 'APeterson12');
 
 --DepartmentTicket Table
 INSERT INTO DepartmentTicket VALUES ('Accounting', 540);
@@ -74,11 +74,11 @@ INSERT INTO TicketHashtag VALUES (540, '#LackPermissions');
 INSERT INTO Comment VALUES (1, 1, '2023-05-13', 'Im unable to access our financial reports on QuickBooks. Im receiving an error message that says Access denied. You do not have the necessary permissions to access this report. Ive tried logging out and logging back in, but the issue persists. Can anyone grant me acess?', 'APeterson12', 540);
 INSERT INTO Comment VALUES (2, 2, '2023-05-14', 'Hi Andrew, Ive updated your user permissions, and you should now be able to access the financial reports. Please let me know if you have any further issues.', 'JamesDavis', 540);
 INSERT INTO Comment VALUES (3, 3, '2023-05-14', 'Thanks, everything looks good now. Im able to access the financial reports without any issues. I really appreciate your help!', 'APeterson12', 540);
-INSERT INTO Comment VALUES (4, 4, '2023-05-14', 'Youre welcome, Andrew. Glad I could help. Dont hesitate to reach out if you have any further issues.', 'JamesDabis', 540);
+INSERT INTO Comment VALUES (4, 4, '2023-05-14', 'Youre welcome, Andrew. Glad I could help. Dont hesitate to reach out if you have any further issues.', 'JamesDavis', 540);
+
+--FAQ Table
+INSERT INTO FAQ VALUES (1, 'Accounting');
 
 --Question Table
 INSERT INTO Question VALUES (1, 1, 'What is the process for managing subscription billing and revenue in our company?','', 1);
 INSERT INTO Question VALUES (2, 2, ' How to comply with tax laws and regulations in foreign countries?','', 1);
-
---FAQ Table
-INSERT INTO FAQ VALUES (1, 'Accounting');
