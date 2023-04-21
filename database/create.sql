3a0e9eeb676b2044f5f65b579891548e0fa79470
Adriano Machado <93844395+Adriano-7@users.noreply.github.com>
2023-04-21 12:34:52 +0100

    Started populating the database

--- database/create.sql
+++ database/create.sql
@@ -2,20 +2,14 @@ PRAGMA foreign_keys = off;
 PRAGMA encoding="UTF-8";
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS Admin;
-CREATE TABLE Admin (
-    username TEXT REFERENCES Agent (username) ON DELETE CASCADE ON UPDATE CASCADE
+DROP TABLE IF EXISTS Department;
+CREATE TABLE Department (
+    name TEXT PRIMARY KEY
 );
 
-DROP TABLE IF EXISTS Agent;
-CREATE TABLE Agent (
-    username REFERENCES Client (username) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-DROP TABLE IF EXISTS AgentQuestion;
-CREATE TABLE AgentQuestion (
-    username REFERENCES Agent (username) ON DELETE SET NULL ON UPDATE CASCADE,
-    quest_id INTEGER REFERENCES Question (quest_id) ON DELETE CASCADE ON UPDATE CASCADE
+DROP TABLE IF EXISTS Hashtag;
+CREATE TABLE Hashtag (
+    name TEXT PRIMARY KEY
 );
 
 DROP TABLE IF EXISTS Client;
@@ -26,44 +20,14 @@ CREATE TABLE Client (
     password TEXT
 );
 
-DROP TABLE IF EXISTS Comment;
-CREATE TABLE Comment (
-    comment_id INTEGER PRIMARY KEY,
-    num INTEGER, date TEXT, 
-    content, 
-    username REFERENCES Client (name) ON DELETE SET NULL ON UPDATE CASCADE NOT NULL, 
-    ticket_id INTEGER REFERENCES Ticket (ticket_id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+DROP TABLE IF EXISTS Agent;
+CREATE TABLE Agent (
+    username REFERENCES Client (username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-DROP TABLE IF EXISTS Department;
-CREATE TABLE Department (
-    name TEXT PRIMARY KEY
-);
-
-DROP TABLE IF EXISTS DepartmentTicket;
-CREATE TABLE DepartmentTicket (
-    name_department REFERENCES Department (name) ON DELETE CASCADE ON UPDATE CASCADE,
-    ticket_id INTEGER REFERENCES Ticket (ticket_id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-DROP TABLE IF EXISTS FAQ;
-CREATE TABLE FAQ (
-    faq_id INTEGER PRIMARY KEY, 
-    name_department REFERENCES Department (name) ON DELETE CASCADE ON UPDATE CASCADE UNIQUE NOT NULL
-);
-
-DROP TABLE IF EXISTS Hashtag;
-CREATE TABLE Hashtag (
-    name TEXT PRIMARY KEY
-);
-
-DROP TABLE IF EXISTS Question;
-CREATE TABLE Question (
-    quest_id INTEGER PRIMARY KEY, 
-    num INTEGER, 
-    title TEXT, 
-    content TEXT,
-    faq_id INTEGER REFERENCES FAQ (faq_id) ON DELETE CASCADE ON UPDATE CASCADE
+DROP TABLE IF EXISTS Admin;
+CREATE TABLE Admin (
+    username TEXT REFERENCES Agent (username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS Ticket;
@@ -77,10 +41,47 @@ CREATE TABLE Ticket (
     username REFERENCES Client (username) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+DROP TABLE IF EXISTS DepartmentTicket;
+CREATE TABLE DepartmentTicket (
+    name_department REFERENCES Department (name) ON DELETE CASCADE ON UPDATE CASCADE,
+    ticket_id INTEGER REFERENCES Ticket (ticket_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 DROP TABLE IF EXISTS TicketHashtag;
 CREATE TABLE TicketHashtag (
     ticket_id INTEGER REFERENCES Ticket (ticket_id) ON DELETE CASCADE ON UPDATE CASCADE, 
     name REFERENCES Hashtag (name)
+);
+
+DROP TABLE IF EXISTS Comment;
+CREATE TABLE Comment (
+    comment_id INTEGER PRIMARY KEY,
+    num INTEGER, date TEXT, 
+    content, 
+    username REFERENCES Client (name) ON DELETE SET NULL ON UPDATE CASCADE NOT NULL, 
+    ticket_id INTEGER REFERENCES Ticket (ticket_id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
+);
+
+
+DROP TABLE IF EXISTS Question;
+CREATE TABLE Question (
+    quest_id INTEGER PRIMARY KEY, 
+    num INTEGER, 
+    title TEXT, 
+    content TEXT,
+    faq_id INTEGER REFERENCES FAQ (faq_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS AgentQuestion;
+CREATE TABLE AgentQuestion (
+    username REFERENCES Agent (username) ON DELETE SET NULL ON UPDATE CASCADE,
+    quest_id INTEGER REFERENCES Question (quest_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS FAQ;
+CREATE TABLE FAQ (
+    faq_id INTEGER PRIMARY KEY, 
+    name_department REFERENCES Department (name) ON DELETE CASCADE ON UPDATE CASCADE UNIQUE NOT NULL
 );
 
 COMMIT TRANSACTION;
---
+++ database/populate.sql
@@ -0,0 +1,74 @@
+PRAGMA foreign_keys = ON;
+
+--Department Table
+INSERT INTO Department VALUES ('Accounting');
+INSERT INTO Department VALUES ('Law');
+INSERT INTO Department VALUES ('Human Resources');
+INSERT INTO Department VALUES ('Sales');
+
+--Hashtag Table
+INSERT INTO Hashtag VALUES ('#LackPermissions');
+
+--Client Table
+INSERT INTO Client VALUES ('Andrew Peterson', 'APeterson12', 'peterson_andrew@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Emily Davis', 'EmilyDavis', 'EmilyDavis15@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Adam Green', 'AGreen', 'Agreen@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Jessica Ramirez', 'JessicaRamirez15', 'jessica_ramirez@snapticket.com','7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Daniel Wilson', 'DWilson14', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('James Davis', 'JamesDavis', 'james_davis7@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Jessica Murphy', 'JMurphy', 'jessica_murphy@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Michael Brown', 'MBrown1', 'michael_brown24@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Sarah Johnson', 'SarahJohnson7', 'sarah_johnson7@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Christopher Chen', 'CChen', 'christopher_chen2@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Samantha Kim', 'SKim', 'SKim@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Andrew Patel', 'APatel', 'APat@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Mattew Cooper', 'MCooper', 'MCooper12@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Ethan Chen', 'EChen', 'EthanChen1@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Benjamin Collins', 'BenjCollins', 'BCollins@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+INSERT INTO Client VALUES ('Tyer Williams', 'TWilliams', 'TWilliams@snapticket.com', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+
+--Agents Table
+INSERT INTO Agent VALUES ('JamesDavis');
+INSERT INTO Agent VALUES ('JMurphy');
+INSERT INTO Agent VALUES ('MBrown1');
+INSERT INTO Agent VALUES ('SarahJohnson7');
+INSERT INTO Agent VALUES ('CChen');
+INSERT INTO Agent VALUES ('SKim');
+INSERT INTO Agent VALUES ('APat');
+INSERT INTO Agent VALUES ('MCooper');
+INSERT INTO Agent VALUES ('EChen');
+INSERT INTO Agent VALUES ('BenjCollins');
+INSERT INTO Agent VALUES ('TWilliams');
+
+--Admin Table
+INSERT INTO Admin VALUES ('JMurphy');
+
+--Ticket Table
+INSERT INTO Ticket VALUES (540, 'Need access to financial reports', '2023-05-13', '', 'SarahJohnson7', 'Assigned', 'APeterson12');
+INSERT INTO Ticket VALUES (541, 'Incorrect tax filling submitted to IRS for client', '2023-05-13', '', 'MBrown1', 'Assigned', 'APeterson12');
+INSERT INTO Ticket VALUES (542, 'Issue with depreciation calculation for fixed assets', '2023-05-13', '', 'SarahJohnson7', 'Assigned', 'APeterson12');
+INSERT INTO Ticket VALUES (543, 'Issue with accounts payable aging report', '2023-05-13', '', '', 'Open', 'APeterson12');
+INSERT INTO Ticket VALUES (544, 'Inquiry about accounting treatment for leasehold improvements', '2023-05-13', '', 'CChen', 'Closed', 'APeterson12');
+INSERT INTO Ticket VALUES (545, 'Issue with expense reimbursement for employee travel', '2023-05-13', '', '', 'Open', 'APeterson12');
+INSERT INTO Ticket VALUES (546, 'Request for assistance with financial due diligence', '2023-05-13', '', 'JamesDavis', 'Closed', 'APeterson12');
+INSERT INTO Ticket VALUES (547, 'Unable to reconcile bank accounts in QuickBooks', '2023-05-13', '', 'SKim', 'Assigned', 'APeterson12');
+INSERT INTO Ticket VALUES (548, 'Having trouble connecting to the company email', '2023-05-13', '', 'APat', 'Assigned', 'APeterson');
+INSERT INTO Ticket VALUES (549, 'Issue with payroll processing', '2023-05-13', '', 'CChen', 'Assigned', 'APeterson');
+
+--DepartmentTicket Table
+INSERT INTO DepartmentTicket VALUES ('Accounting', 540);
+INSERT INTO DepartmentTicket VALUES ('Accounting', 541);
+INSERT INTO DepartmentTicket VALUES ('Accounting', 542);
+INSERT INTO DepartmentTicket VALUES ('Accounting', 543);
+INSERT INTO DepartmentTicket VALUES ('Accounting', 544);
+INSERT INTO DepartmentTicket VALUES ('Accounting', 545);
+INSERT INTO DepartmentTicket VALUES ('Accounting', 546);
+INSERT INTO DepartmentTicket VALUES ('Accounting', 547);
+INSERT INTO DepartmentTicket VALUES ('Accounting', 548);
+INSERT INTO DepartmentTicket VALUES ('Accounting', 549);
+
+--TicketHashtag Table
+INSERT INTO TicketHashtag VALUES (540, '#LackPermissions');
+
+--Comment Table
+INSERT INTO Comment VALUES (1, 1, 'Im unable to access our financial reports on QuickBooks. Im receiving an error message that says Access denied. You do not have the necessary permissions to access this report. Ive tried logging out and logging back in, but the issue persists. Can anyone grant me acess?'
